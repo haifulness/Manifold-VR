1399ae5d1409279467448d63386d6201a077f0f3
Hai Tran <hai.t@outlook.com>
2017-02-10 12:09:00 -0600

    Reflection Probes

---
+++ ReflectionProbe/Assets/Scripts/TouchController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	public OVRInput.Controller Controller;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = OVRInput.GetLocalControllerPosition (Controller);
+		transform.localRotation = OVRInput.GetLocalControllerRotation (Controller);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
